33db3342259f8420566b1afa225f3908cef66eb1
Abinash Rai <abinashbantawa1905@gmail.com>
2024-01-05 04:20:51 +0545

    made a demo file to understand inheritance

---
+++ demo.ts
@@ -0,0 +1,37 @@
+class Persons {
+  name: string;
+  age: number;
+  hobbies: string[];
+
+  constructor(name: string, age: number, hobbies: string[]) {
+    this.name = name;
+    this.age = age;
+    this.hobbies = hobbies;
+  }
+
+  introduce(): string {
+    return `My name is ${this.name}, my age is ${
+      this.age
+    } and my hobbies are ${this.hobbies.join(", ")}`;
+  }
+}
+
+class Students extends Persons {
+  grade: number;
+  constructor(name: string, age: number, hobbies: string[], grade: number) {
+    super((name = name), (age = age), (hobbies = hobbies));
+    this.grade = grade;
+  }
+
+  introduce(): string {
+    return `${super.introduce()}, I'm in grade ${this.grade}`;
+  }
+}
+
+const person1 = new Persons("Mike", 33, ["gaming", "writing"]);
+const person2 = new Persons("Josh", 21, ["cooking", "drawing"]);
+
+const student1 = new Students("Jake", 41, ["running", "reading"], 12);
+
+console.log(person1.introduce());
+console.log(student1.introduce());
